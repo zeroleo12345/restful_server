aae7bb40eb2e093779442a439047e7ad59379aa4
zeroleo12345 <zeroleo12345@163.com>
2018-06-22 23:04:01 +0800

    [ADDED] add print gunicorn access log

--- bin/run_restful_server.sh
+++ bin/run_restful_server.sh
@@ -5,5 +5,5 @@ if [ "$DEBUG" == "True" ]; then
     python manage.py runserver 0.0.0.0:80
 else
     python manage.py migrate
-    gunicorn trade.wsgi --threads 4 -b :80 --access-logfile -
+    gunicorn trade.wsgi --threads 4 -b :80 --access-logfile - --access-logformat '%(h)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(a)s" %(L)s'
 fi
