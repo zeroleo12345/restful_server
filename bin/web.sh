b2721c4e895d528c850e7f23b5598d498fffef48
lyn <zeroleo12345@gmail.com>
2018-10-29 05:08:32 -0700

    [ADDED] 使用supervisord管理进程 (#27)

---
+++ bin/web.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+if [ "$DEBUG" == "True" ]; then
+    python manage.py migrate
+    exec python manage.py runserver 0.0.0.0:8000
+else
+    python manage.py migrate
+    exec gunicorn trade.wsgi --threads 4 -b :8000 --access-logfile - --access-logformat '%(h)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(a)s" %(L)s'
+fi
