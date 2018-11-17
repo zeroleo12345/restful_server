9e65a45c895fb8dc02e01db59b56118bbdb6c3f3
zeroleo12345 <zeroleo12345@163.com>
2018-11-17 16:46:39 +0800

    修复url /开头和$结束问题

--- src/trade/mp/urls.py
+++ src/trade/mp/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from trade.mp import views
 
 mp_urls = [
-    url(r'^echostr$', views.EchoStrView.as_view()),
+    url(r'^/echostr$', views.EchoStrView.as_view()),
 ]
