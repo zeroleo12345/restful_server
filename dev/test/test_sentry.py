01567fd7c13330bcf79e39036f7809364c514a79
lyn <zeroleo12345@gmail.com>
2018-11-22 08:23:13 +0800

    [IMPROVED] mysql内存占用从200m优化到50m (#33)
    * [IMPROVED] mysql内存占用从200m优化到50m
    
    * modify 06:00 dump db
    
    * 同步mysql_dump.py修改
    
    * 关闭SElinux后, 不使用Dockerfile.mysql
    
    * ignore run/mysql
    
    * add test_sentry.py
    
    * backup.sh -> export.sh
    
    * unmount run/mysql; use original config
    
    * sort_buffer_size 由 32K 调整到默认值 256K

---
+++ dev/test/test_sentry.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+# 开发文档地址:    https://docs.sentry.io/error-reporting/quickstart/?platform=python
+
+from decouple import config
+
+# 上报异常
+import sentry_sdk
+from sentry_sdk import capture_exception
+
+SENTRY_DSN = config('SENTRY_DSN')
+sentry_sdk.init(SENTRY_DSN)
+
+try:
+    a = 1 / 0
+except Exception as e:
+    # Alternatively the argument can be omitted
+    capture_exception(e)
+
+# 上报信息
+from sentry_sdk import capture_message
+capture_message('Sentry Hello World')
