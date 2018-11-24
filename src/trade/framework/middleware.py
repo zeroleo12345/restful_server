b3eb75c768bfa785f762a9785a4f1051bd804de8
lyn <zeroleo12345@gmail.com>
2018-11-24 11:23:48 +0800

    [FIXED] 修复user in request报错 (#35)
    * [FIXED] 修复user in request报错

--- src/trade/framework/middleware.py
+++ src/trade/framework/middleware.py
@@ -33,7 +33,7 @@ class TokenSetMiddleware(object):
         response = self.get_response(request)
 
         token = request.META.get('HTTP_AUTHORIZATION', '')
-        if UserPermission.is_user(request.user) and not token:
+        if not token and getattr(request, 'user', None) and UserPermission.is_user(request.user):
             response['Authorization'] = JWTAuthentication.jwt_encode_handler(request.user)
 
         return response
