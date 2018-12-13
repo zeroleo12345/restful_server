f1e024d69d21aef1139343f5946abe5cd76473fa
zeroleo12345 <zeroleo12345@163.com>
2018-12-13 16:23:32 +0800

    修复累计Sum问题

--- src/trade/management/commands/statistics.py
+++ src/trade/management/commands/statistics.py
@@ -20,20 +20,24 @@ class Command(BaseCommand):
 
 
 class ServiceLoop(Service):
-    interval = 600
+    interval = 0
 
     def run(self):
         # 累计昨天成交
         today = timezone.localtime().replace(hour=0, minute=0, second=0, microsecond=0)
         yesterday = today - datetime.timedelta(days=1)
-        today_total = Orders.objects.filter(
+        today_sum = Orders.objects.filter(
             status='paid', updated_at__gt=yesterday, updated_at__lt=today
-        ).aggregate(Sum('total_fee'))['total_fee']
+        ).aggregate(sum=Sum('total_fee'))['sum']
+        if not today_sum:
+            today_sum = 0
 
         # 累计所有成交
-        total = Orders.objects.filter(status='paid').aggregate(Sum('total_fee'))['total_fee']
+        total_sum = Orders.objects.filter(status='paid').aggregate(sum=Sum('total_fee'))['sum']
+        if not total_sum:
+            total_sum = 0
 
         # 发送slack统计消息
-        text = f'今天充值金额: {today_total/100}元, 历史累计充值金额: {total/100}元'
+        text = f'今天充值金额: {today_sum/100} 元, 历史累计充值金额: {total_sum/100} 元'
         send_slack_message(text=text)
         self.term = 1
