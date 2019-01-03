8bb55c2b267f0cd16b1cf4eac3634f90533aaf7b
zeroleo12345 <zeroleo12345@163.com>
2019-01-03 15:53:25 +0800

    m

--- src/trade/order/urls.py
+++ src/trade/order/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from trade.order import views
 
 order_urls = [
-    url(r'^$', views.OrderView.as_view()),
-    url(r'^/notify$', views.OrderNotifyView.as_view()),
+    url(r'^$', views.OrderView.as_view()),      # url如: http://localhost/order
+    url(r'^/notify$', views.OrderNotifyView.as_view()),     # url如: http://localhost/order/notify
 ]
