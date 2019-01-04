e3a44623c7ca6dbeacf47024ae8082bca2a21518
zeroleo12345 <zeroleo12345@163.com>
2019-01-04 17:46:57 +0800

    m

--- dev/runtest_in_docker.sh
+++ dev/runtest_in_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -o verbose
 
-docker-compose exec web ./runtest
+docker-compose exec web ../.test/runtest
 
 # docker-compose exec web pytest  trade/user/tests.py
 # docker-compose exec web pytest  -k  test_payjs_post
