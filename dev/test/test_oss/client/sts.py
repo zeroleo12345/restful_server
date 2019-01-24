ca6bb5f580f4f1453083a7640d9f2d4edb601d50
zeroleo12345 <zeroleo12345@163.com>
2019-01-24 19:57:31 +0800

    调整代码

--- dev/test/test_oss/client/sts.py
+++ dev/test/test_oss/client/sts.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import json
-import os
 # 第三方库
 from aliyunsdkcore import client
 from aliyunsdksts.request.v20150401 import AssumeRoleRequest
@@ -20,27 +19,35 @@ import oss2
 #   http://oss-cn-hangzhou.aliyuncs.com
 #   https://oss-cn-hangzhou.aliyuncs.com
 # 分别以HTTP、HTTPS协议访问。
-"""
-:param OSS_ACCESS_KEY_ID: 子用户的 access key id
-:param OSS_ACCESS_KEY_SECRET: 子用户的 access key secret
-:param OSS_ARN: STS角色的Arn
-"""
-OSS_ACCESS_KEY_ID = os.getenv('OSS_ACCESS_KEY_ID')
-OSS_ACCESS_KEY_SECRET = os.getenv('OSS_ACCESS_KEY_SECRET')
-OSS_BUCKET_NAME = os.getenv('OSS_BUCKET_NAME')
-OSS_REGION = os.getenv('OSS_REGION', 'cn-hongkong')
-OSS_ENDPOINT = os.getenv('OSS_ENDPOINT')      # EndPoint（地域节点）
-OSS_ARN = os.getenv('OSS_ARN')      # Role Arn      https://ram.console.aliyun.com/roles
-OSS_CALLBACK_URL = os.getenv('OSS_CALLBACK_URL')
 
 
-def fetch_sts_token():
-    clt = client.AcsClient(OSS_ACCESS_KEY_ID, OSS_ACCESS_KEY_SECRET, OSS_REGION)
+def get_x_oss_callback(oss_callback_url):
+    # 准备回调参数，更详细的信息请参考 https://help.aliyun.com/document_detail/31989.html
+    callback_dict = {
+        'callbackUrl': f'{oss_callback_url}/debug',
+        # 'callbackHost': 'oss-cn-hangzhou.aliyuncs.com',
+
+        # 'callbackBodyType': 'application/x-www-form-urlencoded',
+        # 'callbackBody': 'object=${object}&size=${size}&mimeType=${mimeType}',
+
+        'callbackBodyType': 'application/json',
+        'callbackBody': '''{"bucket": ${bucket}, "object": ${object}, "etag": ${etag}, "size": ${size}}''',
+    }
+    # 回调参数是json格式，并且base64编码
+    callback_param = json.dumps(callback_dict).strip()
+    x_oss_callback = oss2.utils.b64encode_as_string(callback_param)
+    return x_oss_callback
+
+
+def fetch_sts_token(key, oss_access_key_id, oss_access_key_secret, oss_arn, oss_bucket_name, oss_region):
+    clt = client.AcsClient(oss_access_key_id, oss_access_key_secret, oss_region)
     req = AssumeRoleRequest.AssumeRoleRequest()
 
     req.set_accept_format('json')
-    req.set_RoleArn(OSS_ARN)
+    req.set_RoleArn(oss_arn)
     req.set_RoleSessionName('rethink-backend')
+    resource = f'["acs:oss:*:*:{oss_bucket_name}/{key}"]'
+    req.set_Policy('{"Statement": [ { "Action": [ "oss:*" ], "Effect": "Allow", "Resource": ' + resource + ' } ], "Version": "1" }')
 
     body = clt.do_action_with_exception(req)
 
@@ -63,16 +70,32 @@ def fetch_sts_token():
     return access_key_id, access_key_secret, security_token, request_id, expiration
 
 
-access_key_id, access_key_secret, security_token, request_id, expiration = fetch_sts_token()
-print(f'STS access_key_id: {access_key_id}, access_key_secret: {access_key_secret}')
-print(f'STS security_token: {security_token}, request_id: {request_id}, expiration: {expiration}')
+def upload_and_callback(key, access_key_id, access_key_secret, security_token, x_oss_callback, oss_endpoint, oss_bucket_name):
+    # 参考:  https://yq.aliyun.com/articles/68863
+    # 方式1: 回调参数放在消息头中的 x-oss-callback。这种方式比较常用，推荐该方式；
+    headers = {'x-oss-callback': x_oss_callback}
+    # 方式2: 通过 QueryString 中的参数callback携带回调参数. (例如callback=x_oss_callback).。
+    pass
+
+    # 方式1: 非临时授权
+    # auth = oss2.Auth(access_key_id, access_key_secret)
+    # bucket = oss2.Bucket(auth, OSS_ENDPOINT, oss_bucket_name)
+
+    # 方式2: 客户端临时授权
+    auth = oss2.StsAuth(access_key_id, access_key_secret, security_token)
+    # 创建Bucket对象，所有Object相关的接口都可以通过Bucket对象来进行
+    bucket = oss2.Bucket(auth, oss_endpoint, oss_bucket_name)
+
+    # 上传
+    response = bucket.put_object(key, "Anything you're good at contributes to happiness.", headers)
+    print(f'api server response: ', response.resp.read())
 
 
-def upload(access_key_id, access_key_secret, security_token):
+def upload(access_key_id, access_key_secret, security_token, oss_endpoint, oss_bucket_name):
     # 客户端使用临时授权
     auth = oss2.StsAuth(access_key_id, access_key_secret, security_token)
     # 创建Bucket对象，所有Object相关的接口都可以通过Bucket对象来进行
-    bucket = oss2.Bucket(auth, OSS_ENDPOINT, OSS_BUCKET_NAME)
+    bucket = oss2.Bucket(auth, oss_endpoint, oss_bucket_name)
 
     # 上传一段字符串。Object名是motto.txt，内容是一段名言。
     bucket.put_object('motto.txt', 'Never give up. - Jack Ma')
@@ -82,41 +105,25 @@ def upload(access_key_id, access_key_secret, security_token):
     # bucket.delete_object('motto.txt')
 
 
-def upload_and_callback(access_key_id, access_key_secret, security_token):
-    # 准备回调参数，更详细的信息请参考 https://help.aliyun.com/document_detail/31989.html
-    callback_dict = {
-        'callbackUrl': f'{OSS_CALLBACK_URL}/debug',
-        # 'callbackHost': 'oss-cn-hangzhou.aliyuncs.com',
+if __name__ == "__main__":
+    import os
+    """
+    :param OSS_ACCESS_KEY_ID: 子用户的 access key id
+    :param OSS_ACCESS_KEY_SECRET: 子用户的 access key secret
+    :param OSS_ARN: STS角色的Arn
+    """
+    OSS_ACCESS_KEY_ID = os.getenv('OSS_ACCESS_KEY_ID')
+    OSS_ACCESS_KEY_SECRET = os.getenv('OSS_ACCESS_KEY_SECRET')
+    OSS_BUCKET_NAME = os.getenv('OSS_BUCKET_NAME')
+    OSS_REGION = os.getenv('OSS_REGION', 'cn-hongkong')
+    OSS_ENDPOINT = os.getenv('OSS_ENDPOINT')      # EndPoint（地域节点）
+    OSS_ARN = os.getenv('OSS_ARN')      # Role Arn      https://ram.console.aliyun.com/roles
+    OSS_CALLBACK_URL = os.getenv('OSS_CALLBACK_URL')
+    KEY = 'file/quote.txt'
 
-        # 'callbackBodyType': 'application/x-www-form-urlencoded',
-        # 'callbackBody': 'object=${object}&size=${size}&mimeType=${mimeType}',
+    access_key_id, access_key_secret, security_token, request_id, expiration = fetch_sts_token(KEY, OSS_ACCESS_KEY_ID, OSS_ACCESS_KEY_SECRET, OSS_ARN, OSS_BUCKET_NAME, OSS_REGION)
+    x_oss_callback = get_x_oss_callback(OSS_CALLBACK_URL)
+    print(f'STS access_key_id: {access_key_id}, access_key_secret: {access_key_secret}')
+    print(f'STS security_token: {security_token}, request_id: {request_id}, expiration: {expiration}')
 
-        'callbackBodyType': 'application/json',
-        'callbackBody': '''{"bucket": ${bucket}, "object": ${object}, "etag": ${etag}, "size": ${size}}''',
-    }
-    # 回调参数是json格式，并且base64编码
-    callback_param = json.dumps(callback_dict).strip()
-    x_oss_callback = oss2.utils.b64encode_as_string(callback_param)
-
-    # 参考:  https://yq.aliyun.com/articles/68863
-    # 方式1: 回调参数放在消息头中的 x-oss-callback。这种方式比较常用，推荐该方式；
-    headers = {'x-oss-callback': x_oss_callback}
-    # 方式2: 通过 QueryString 中的参数callback携带回调参数. (例如callback=x_oss_callback).。
-    pass
-
-    # 方式1: 非临时授权
-    # auth = oss2.Auth(access_key_id, access_key_secret)
-    # bucket = oss2.Bucket(auth, OSS_ENDPOINT, OSS_BUCKET_NAME)
-
-    # 方式2: 客户端临时授权
-    auth = oss2.StsAuth(access_key_id, access_key_secret, security_token)
-    # 创建Bucket对象，所有Object相关的接口都可以通过Bucket对象来进行
-    bucket = oss2.Bucket(auth, OSS_ENDPOINT, OSS_BUCKET_NAME)
-
-    # 上传
-    key = 'file/quote.txt'
-    response = bucket.put_object(key, "Anything you're good at contributes to happiness.", headers)
-    print(f'api server response: ', response.resp.read())
-
-
-upload_and_callback(access_key_id, access_key_secret, security_token)
+    upload_and_callback(KEY, access_key_id, access_key_secret, security_token, x_oss_callback, OSS_ENDPOINT, OSS_BUCKET_NAME)
