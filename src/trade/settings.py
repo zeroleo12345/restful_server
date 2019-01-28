0c15a98353f952af3960aae13029112647aab5db
zeroleo12345 <zeroleo12345@163.com>
2019-01-28 13:52:37 +0800

    [ADDED] 移除DEBUG MIDDLEWARE

--- src/trade/settings.py
+++ src/trade/settings.py
@@ -75,14 +75,15 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
-if DEBUG:
-    MIDDLEWARE += [
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    ]
+
+# if DEBUG:
+#     MIDDLEWARE += [
+#         'django.contrib.sessions.middleware.SessionMiddleware',
+#         'django.middleware.csrf.CsrfViewMiddleware',
+#         'django.contrib.auth.middleware.AuthenticationMiddleware',
+#         'django.contrib.messages.middleware.MessageMiddleware',
+#         'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#     ]
 
 ROOT_URLCONF = 'trade.urls'
 
