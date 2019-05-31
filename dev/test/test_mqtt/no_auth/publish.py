5c2aa398c051b0eddca2e162968a28311712528f
zeroleo12345 <zeroleo12345@163.com>
2019-05-31 10:19:14 +0800

    删除dev/test/test_oss, test_mqtt, test_rabbitmq, test_sentry, test_slack; 移动到mytest仓库

--- dev/test/test_mqtt/no_auth/publish.py
+++
@@ -1,7 +0,0 @@
-# import paho.mqtt.client as mqtt
-# client = mqtt.Client()
-# client.connect("127.0.0.1", 1883, 600)
-# client.publish('emqtt', payload='Hello, EMQ!', qos=0)
-
-import paho.mqtt.publish as publish
-publish.single('emqtt', payload='Hello, EMQ!', qos=0)
