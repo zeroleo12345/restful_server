e7f51c5006b83a82d9b1bb28547c90d714ab399a
zeroleo12345 <zeroleo12345@163.com>
2020-01-29 18:00:40 +0800

    fix

--- tools/flake8_in_host.sh
+++ tools/flake8_in_host.sh
@@ -4,4 +4,4 @@ set -o verbose
 # pip install flake8
 
 # flake8 src/ --show-source --exclude=migrations,manage.py --max-line-length=120
-flake8 src --config=src/tox.ini
+flake8 src --config=.test/flake8.ini
