f564b8d06c4772c55b93ae8108e606bfd0628a51
zeroleo12345 <zeroleo12345@163.com>
2020-01-30 10:27:23 +0800

    [ADDED] 增加定义SETTINGS_FILE_FOR_DYNACONF

--- bin/crontab/export.sh
+++ bin/crontab/export.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-if [ "$DEBUG" == "True" ]; then
+if [[ "$DEBUG" == "True" || "$DEBUG" == "1" ]]; then
     sleep 360
 else
     python /app/src/trade/management/commands/mysql_dump.py -host mysql -P 3306 -u root -p 'root' -db trade -dir '/app/run/backup/'
