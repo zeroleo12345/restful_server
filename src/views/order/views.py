149a36bee96fd1749d934353b58d574ebfd056be
zeroleo12345 <zeroleo12345@163.com>
2020-01-31 18:02:37 +0800

    fix

--- src/views/order/views.py
+++ src/views/order/views.py
@@ -69,33 +69,22 @@ class OrderNotifyView(APIView):
 
     # /order/notify     付款结果通知
     def post(self, request):
-        # 1. 解析微信侧回调请求
+        log.d(f'we_pay notify. data: {request.body}')
         try:
-            # data: OrderedDict([(u'appid', u'wx7f843ee17bc2a7b7'), (u'attach', u'{"btype": 0}'), (u'bank_type', u'CFT'), (u'cash_fee', 1), (u'fee_type', u'CNY'), (u'is_subscribe', u'Y'), (u'mch_id', u'1480215992'), (u'nonce_str', u'bZhA1HTmIqBFCluKpai32Yj97tvk4DzV'), (u'openid', u'ovj3E0l9vffwBuqz_PNu25yL_is4'), (u'out_trade_no', u'15021710481087kJMqd36CBz9OqFnK'), (u'result_code', u'SUCCESS'), (u'return_code', u'SUCCESS'), (u'time_end', u'20170808134526'), (u'total_fee', 1), (u'trade_type', u'JSAPI'), (u'transaction_id', u'4005762001201708085135613047'), (u'sign', u'F59843FFFAE5E70A9F1B67D755A372E0')])
-            # SUCCESS 和 FAIL: out_trade_no, attach附加值
-            xml = request.body
+            # request.data: OrderedDict([(u'appid', u'wx7f843ee17bc2a7b7'), (u'attach', u'{"btype": 0}'), (u'bank_type', u'CFT'), (u'cash_fee', 1), (u'fee_type', u'CNY'), (u'is_subscribe', u'Y'), (u'mch_id', u'1480215992'), (u'nonce_str', u'bZhA1HTmIqBFCluKpai32Yj97tvk4DzV'), (u'openid', u'ovj3E0l9vffwBuqz_PNu25yL_is4'), (u'out_trade_no', u'15021710481087kJMqd36CBz9OqFnK'), (u'result_code', u'SUCCESS'), (u'return_code', u'SUCCESS'), (u'time_end', u'20170808134526'), (u'total_fee', 1), (u'trade_type', u'JSAPI'), (u'transaction_id', u'4005762001201708085135613047'), (u'sign', u'F59843FFFAE5E70A9F1B67D755A372E0')])
             # (Pdb) request.body
             # b'<xml><appid><![CDATA[wx54d296959ee50c0b]]></appid>\n<attach><![CDATA[{"tariff_name": "month1"}]]></attach>\n<bank_type><![CDATA[CFT]]></bank_type>\n<cash_fee><![CDATA[1]]></cash_fee>\n<fee_type><![CDATA[CNY]]></fee_type>\n<is_subscribe><![CDATA[Y]]></is_subscribe>\n<mch_id><![CDATA[1517154171]]></mch_id>\n<nonce_str><![CDATA[dJ1t73xXmCrOjn8z5DP6BKyNqgI0cwvS]]></nonce_str>\n<openid><![CDATA[o0FSR0Zh3rotbOog_b2lytxzKrYo]]></openid>\n<out_trade_no><![CDATA[1540483879395x3Oko4Ta9RWamsQCW]]></out_trade_no>\n<result_code><![CDATA[SUCCESS]]></result_code>\n<return_code><![CDATA[SUCCESS]]></return_code>\n<sign><![CDATA[22BE162C29D8558541F04475C379E18B]]></sign>\n<time_end><![CDATA[20181026001122]]></time_end>\n<total_fee>1</total_fee>\n<trade_type><![CDATA[JSAPI]]></trade_type>\n<transaction_id><![CDATA[4200000206201810263667544938]]></transaction_id>\n</xml>'
-            data = WePay.parse_payment_result(xml)
+            data = WePay.parse_payment_result(request.body)
+            out_trade_no = data['out_trade_no']
+            attach = data['attach']
+            return_code = data['return_code']
+            assert return_code == 'SUCCESS'
+            transaction_id = data['transaction_id']
+            total_fee = data['total_fee']
+            # 增加用户免费资源
+            increase_user_resource(total_fee, out_trade_no, transaction_id, attach)
+            return self.SUCCESS_RESPONSE
         except (InvalidSignatureException, Exception) as e:
+            log.e(f'we_pay notify error, body: {request.body}')
             sentry_sdk.capture_exception(e)
             return self.SUCCESS_RESPONSE
-
-        out_trade_no = data['out_trade_no']
-        attach = data['attach']
-        log.i(f'wepay order notify, out_trade_no: {out_trade_no}, attach: {attach}')
-
-        return_code = data['return_code']
-        if return_code != 'SUCCESS':
-            return_msg = data.get('return_msg', '')
-            log.e(f'wepay fail, return_msg: {return_msg}')
-            return self.SUCCESS_RESPONSE
-
-        openid = data['openid']
-        transaction_id = data['transaction_id']
-        total_fee = data['total_fee']
-        log.i(f'openid: {openid}, transaction_id: {transaction_id}, total_fee: {total_fee}')
-
-        # 增加用户免费资源
-        increase_user_resource(total_fee, out_trade_no, transaction_id, attach)
-        return self.SUCCESS_RESPONSE
