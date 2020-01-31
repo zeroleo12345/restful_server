c8b9ed2e19d981c7681d979938bf9c4eb4b683af
zeroleo12345 <zeroleo12345@163.com>
2020-01-31 18:28:02 +0800

    fix

--- src/views/order/test/test_order_notify.py
+++ src/views/order/test/test_order_notify.py
@@ -56,37 +56,3 @@ def test_payjs_notify_success():
     assert json_resposne['data']['status'] == 'working'
     expired_at = timezone.localtime(parse_datetime(json_resposne['data']['expired_at']))
     assert expired_at >= timezone.localtime() + relativedelta(months=1) - relativedelta(hours=1)
-
-
-@pytest.mark.skip(reason="手动触发测试")
-def test_payjs_notify_order_not_exist():
-    client = UnitTestAPIClient()
-    user, authorization = UserFactory.new_user_and_authorization(client)
-    data = {
-        'attach': '{"tariff_name": "month1"}', 'mchid': '1511573911', 'openid': 'o7LFAwUGHPZxyNahwjoNQtKh8EME',
-        'out_trade_no': '1534167177710ovfltv6a8v7BsFAH0',
-        'return_code': '1', 'time_end': '2018-08-13 21:33:02', 'total_fee': '1',
-        'transaction_id': '4200000149201808138100178561', 'sign': '061BC78497952DB19B3F337760A95647'
-    }
-
-    # 充值状态通知
-    client = UnitTestAPIClient(authorization=authorization)
-    response = client.post('/order/notify', data=data, format=None)
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.data == 'invalid_order'
-
-
-@pytest.mark.skip(reason="手动触发测试")
-def test_payjs_notify_sign_error():
-    client = UnitTestAPIClient()
-    user, authorization = UserFactory.new_user_and_authorization(client)
-    client = UnitTestAPIClient(authorization=authorization)
-    data = {
-        'attach': ['{"tariff_name": "month1"}'], 'mchid': ['1511573911'], 'openid': ['o7LFAwUGHPZxyNahwjoNQtKh8EME'],
-        'out_trade_no': ['1534167177710ovfltv6a8v7BsFAH0'], 'payjs_order_id': ['2018081321325600636471374'],
-        'return_code': ['1'], 'time_end': ['2018-08-13 21:33:02'], 'total_fee': ['1'],
-        'transaction_id': ['4200000149201808138100178561'], 'sign': ['this is error sign']
-    }
-    response = client.post('/order/notify', data=data, format=None)     # 以 Form 形式
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.data == 'invalid_signature'
