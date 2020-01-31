0f2c516dd77fbd2cd2c922c79f5dd53b7419b23d
zeroleo12345 <zeroleo12345@163.com>
2020-01-31 11:21:56 +0800

    fix

--- src/models/factories/user.py
+++ src/models/factories/user.py
@@ -1,19 +1,9 @@
-import factory
 from rest_framework import status
 #
 from models import User
-from framework.authorization import JWTAuthentication
 
 
-class UserFactory(factory.DjangoModelFactory):
-    class Meta:
-        model = User
-
-    username = factory.Sequence(lambda n: f'username_{n}')
-    password = factory.Sequence(lambda n: f'password_{n}')
-    is_enable = True
-    role = factory.Iterator([role[0] for role in User.ROLE])
-
+class UserFactory(object):
     @classmethod
     def new_user_and_authorization(cls, client):
         # 发送注册验证码
