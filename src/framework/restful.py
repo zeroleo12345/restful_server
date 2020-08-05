6c73033d70ede88cb59350819e709ceaf9b881fa
zeroleo12345 <zeroleo12345@163.com>
2020-08-05 18:35:38 +0800

    m

--- src/framework/restful.py
+++ src/framework/restful.py
@@ -22,7 +22,7 @@ class BihuResponse(JsonResponse):
                 'code': 'ok',
                 'data': data,
             }
-        super(self.__class__, self).__init__(data, safe=safe, json_dumps_params={'ensure_ascii': False}, **kwargs)
+        super(self.__class__, self).__init__(data, safe=safe, json_dumps_params={'ensure_ascii': False}, charset='utf-8', **kwargs)
 
 
 class MyJSONRenderer(JSONRenderer):
