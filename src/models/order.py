1f7b6f82be27919dc3f3fb6ace7820b0945c4f47
zeroleo12345 <zeroleo12345@163.com>
2020-08-05 21:28:03 +0800

    m

--- src/models/order.py
+++ src/models/order.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-import uuid
 # 第三方库
 from framework.database import models, BaseModel
 from framework.field import BaseEnum
@@ -17,7 +16,6 @@ class BroadBandOrder(models.Model, BaseModel):
         EXPIRED = 'expired'     # 已过期
 
     id = models.AutoField(primary_key=True)
-    uuid = models.UUIDField(editable=False, default=uuid.uuid4)
     user_id = models.IntegerField(null=False)
     openid = models.CharField(max_length=255)
     out_trade_no = models.CharField(max_length=255, unique=True)        # 商家订单号
