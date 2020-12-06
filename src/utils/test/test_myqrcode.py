b0763bd167795ee46501a609805edc3bce9df93b
zeroleo12345 <zeroleo12345@163.com>
2020-12-06 20:36:23 +0800

    add

--- src/utils/test/test_myqrcode.py
+++ src/utils/test/test_myqrcode.py
@@ -3,6 +3,6 @@ from utils.myqrcode import add_logo_on_qrcode_from_text
 
 if __name__ == '__main__':
     text = 'http://www.baidu.com'
-    save_path = 'qrcode.png'     # 生成带有图标的二维码
-    logo_path = 'logo.jpeg'      # 用于填充的图标
+    save_path = 'out.png'
+    logo_path = 'logo.jpeg'
     add_logo_on_qrcode_from_text(qrcode_text=text, save_path=save_path, logo_path=logo_path)
