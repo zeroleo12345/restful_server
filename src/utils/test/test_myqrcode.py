04a6b7a2387902643d9b7b4609c72ee26c03d01f
zeroleo12345 <zeroleo12345@163.com>
2020-12-06 20:33:54 +0800

    fix

--- src/utils/test/test_myqrcode.py
+++ src/utils/test/test_myqrcode.py
@@ -1,6 +1,4 @@
 from utils.myqrcode import add_logo_on_qrcode_from_text
-from PIL import Image
-import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
@@ -8,7 +6,3 @@ if __name__ == '__main__':
     save_path = 'qrcode.png'     # 生成带有图标的二维码
     logo_path = 'logo.jpeg'      # 用于填充的图标
     add_logo_on_qrcode_from_text(qrcode_text=text, save_path=save_path, logo_path=logo_path)
-    # 打开图片
-    img = Image.open(save_path)
-    plt.imshow(img)
-    plt.show()
