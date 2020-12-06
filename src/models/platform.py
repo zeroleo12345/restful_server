d73d40355fa09d231929817b088d6fd361cb1473
zeroleo12345 <zeroleo12345@163.com>
2020-12-06 22:40:06 +0800

    m

---
+++ src/models/platform.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+#
+from framework.database import models, BaseModel
+
+
+class Platform(models.Model, BaseModel):
+    class Meta:
+        app_label = 'platform'
+        db_table = 'broadband_user'
+
+    id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField(null=False)
+    ssid = models.CharField(max_length=255, null=True)
+    #
+    created_at = models.DateTimeField(auto_now_add=True)    # auto_now_add only generated on 新创建
+    updated_at = models.DateTimeField(auto_now=True)        # auto_now is generated on 每次修改
+
+    @classmethod
+    def get(cls, id):
+        obj = cls.objects.filter(id=id).first()
+        if not obj:
+            return None
+        return obj
