75374f34fdae5ced3d2a5a18e7200515719a3ede
zeroleo12345 <zeroleo12345@163.com>
2020-12-06 20:14:26 +0800

    add

--- src/utils/myqrcode.py
+++ src/utils/myqrcode.py
@@ -1,49 +1,50 @@
 import os
-import sys
+# 第三方库
+import requests
 import qrcode
 from PIL import Image
 
 
-def add_logo_on_qrcode_from_web(url, path, logo=''):
+def add_logo_on_qrcode_from_web(qrcode_text, logo_url, save_path):
+    response = requests.get(logo_url)
+    # response.content
     pass
 
 
-def add_logo_on_qrcode_from_text(text, save_path, logo_path=''):
+def add_logo_on_qrcode_from_text(qrcode_text, logo_path, save_path):
     """
     生成中间带logo的二维码
-    :param text: 二维码字符串
+    :param qrcode_text: 二维码字符串
     :param save_path: 生成的二维码保存路径
     :param logo_path: logo文件路径
     :return:
     """
+    if not os.path.exists(logo_path):
+        return
+    img_icon = Image.open(logo_path)
     # 初步生成二维码图像
     qr = qrcode.QRCode(version=2, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=8, border=1)
-    qr.add_data(text)
+    qr.add_data(qrcode_text)
     qr.make(fit=True)
     img = qr.make_image()
     # 互动二Image实例并把颜色模式转换成RGBA
     img = img.convert('RGBA')
-    if logo_path and os.path.exists(logo_path):
-        try:
-            icon = Image.open(logo_path)    # 打开logo文件
-            img_w, img_h = img.size
-        except Exception as e:
-            print(e)
-            sys.exit(1)
-        factor = 4
-        #  计算logo尺寸
-        size_w = int(img_w / factor)
-        size_h = int(img_h / factor)
-        #  比较并重新设置logo文件（图片pdsu.png）的尺寸
-        icon_w, icon_h = icon.size
-        if icon_w > size_w:
-            icon_w = size_w
-        if icon_h > size_h:
-            icon_h = size_h
-        icon = icon.resize((icon_w, icon_h), Image.ANTIALIAS)
-        #  计算logo的位置，并且复制到二维码中
-        w = int((img_w - icon_w) / 2)
-        h = int((img_h - icon_h) / 2)
-        icon = icon.convert('RGBA')
-        img.paste(icon, (w, h), icon)
-    img.save(save_path)           # 保存二维码
+    img_w, img_h = img.size
+    factor = 4
+    # 计算logo尺寸
+    size_w = int(img_w / factor)
+    size_h = int(img_h / factor)
+    # 比较并重新设置logo文件（图片pdsu.png）的尺寸
+    icon_w, icon_h = img_icon.size
+    if icon_w > size_w:
+        icon_w = size_w
+    if icon_h > size_h:
+        icon_h = size_h
+    img_icon = img_icon.resize((icon_w, icon_h), Image.ANTIALIAS)
+    # 计算logo的位置，并且复制到二维码中
+    w = int((img_w - icon_w) / 2)
+    h = int((img_h - icon_h) / 2)
+    img_icon = img_icon.convert('RGBA')
+    img.paste(img_icon, (w, h), img_icon)
+    # 保存二维码
+    img.save(save_path)
--- src/utils/test/test_myqrcode.py
+++ src/utils/test/test_myqrcode.py
@@ -1,7 +1,14 @@
 from ..myqrcode import add_logo_on_qrcode_from_text
+from PIL import Image
+import matplotlib.pyplot as plt
+
 
 if __name__ == '__main__':
     text = 'http://www.baidu.com'
     save_path = 'qrcode.png'     # 生成带有图标的二维码
     logo_path = 'logo.jpeg'      # 用于填充的图标
-    add_logo_on_qrcode_from_text(text, save_path, logo_path)
+    add_logo_on_qrcode_from_text(qrcode_text=text, save_path=save_path, logo_path=logo_path)
+    # 打开图片
+    img = Image.open(save_path)
+    plt.imshow(img)
+    plt.show()
