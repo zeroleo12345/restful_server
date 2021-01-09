1449001cc0fcf404ce836365a64ab8f86d14f4dd
zeroleo12345 <zeroleo12345@163.com>
2021-01-09 18:33:14 +0800

    m

--- tools/runtest_in_docker.sh
+++ tools/runtest_in_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -o verbose
 
-docker-compose exec api ../.test/runtest
+docker-compose exec api ./.test/runtest
 
 # docker-compose exec api pytest  trade/user/tests.py
 # docker-compose exec api pytest  -k  test_payjs_post
