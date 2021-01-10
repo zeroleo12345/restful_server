46f0b512868c4eafd7602b892a54cdff7d4e0f47
zeroleo12345 <zeroleo12345@163.com>
2021-01-10 20:38:42 +0800

    m

--- src/views/search/views.py
+++ src/views/search/views.py
@@ -1,20 +1,16 @@
 from rest_framework.views import APIView
 # 项目库
-from framework.authorization import JWTAuthentication
 from framework.restful import BihuResponse
-from controls.auth import Authentication
 from models import Account
 from . import validators
 
 
 class SearchUserView(APIView):
-    authentication_classes = (JWTAuthentication, )      # 默认配置
+    authentication_classes = ()      # 默认配置
     permission_classes = ()
 
     # /search/user?name=abc
     def get(self, request):
-        auth = Authentication(request)
-        #
         serializer = validators.SearchValidator(data=request.GET)
         serializer.is_valid(raise_exception=True)
         name = serializer.validated_data.get('name')
