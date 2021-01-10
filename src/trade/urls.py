0a689a75afc367dfe7efde6f841cf7a16a214a83
zeroleo12345 <zeroleo12345@163.com>
2021-01-10 16:38:33 +0800

    m

--- src/trade/urls.py
+++ src/trade/urls.py
@@ -3,6 +3,7 @@ from views.user.urls import user_urls
 from views.resource.urls import resource_urls
 from views.order.urls import order_urls
 from views.mp.urls import mp_urls
+from views.search.urls import search_urls
 from views.heartbeat.views import HeartBeatView
 from views.debug.views import DebugView
 
@@ -13,4 +14,5 @@ urlpatterns = [
     path(r'user', include(user_urls)),
     path(r'resource', include(resource_urls)),
     path(r'order', include(order_urls)),
+    path(r'search', include(search_urls)),
 ]
--- src/views/search/urls.py
+++ src/views/search/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-resource_urls = [
+search_urls = [
     path(r'', views.SearchView.as_view()),
 ]
