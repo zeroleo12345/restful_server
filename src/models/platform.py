6ce4e6b00f2298fc00dc83d34d3ae5144d18fcfe
zeroleo12345 <zeroleo12345@163.com>
2021-01-10 20:57:42 +0800

    m

--- src/models/platform.py
+++ src/models/platform.py
@@ -14,7 +14,7 @@ class Platform(models.Model, BaseModel):
     id = models.AutoField(primary_key=True)
     owner_user_id = models.IntegerField()
     ssid = models.CharField(max_length=255, null=True)
-    qrcode_url = models.URLField(max_length=512, null=True)
+    qrcode_content = models.URLField(max_length=512, null=True)     # 二维码内容, 例如: http://weixin.qq.com/q/02SE2_xxx
     #
     created_at = models.DateTimeField(auto_now_add=True)    # auto_now_add only generated on 新创建
     updated_at = models.DateTimeField(auto_now=True)        # auto_now is generated on 每次修改
