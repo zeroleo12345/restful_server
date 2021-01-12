d9376fe97e050e0b9fc183e999617d1d797a7353
zeroleo12345 <zeroleo12345@163.com>
2021-01-12 20:43:12 +0800

    Revert "[IMPROVED] 修改宽带到期提醒消息模板ID为: SUx74ExnoOoiESmA-K-pDrHa6qhYce99upCTKGX2Lgk"
    This reverts commit f1ce271b713658ad9e5b2f9e8ac3bbb55c62225c.

--- src/service/wechat/tests/test_wechat.py
+++ src/service/wechat/tests/test_wechat.py
@@ -10,9 +10,9 @@ def usage():
     from service.wechat.tests.test_wechat import *; a = Test()
     
     openid = 'o0FSR0Zh3rotbOog_b2lytxzKrYo'
-    template_id = 'SUx74ExnoOoiESmA-K-pDrHa6qhYce99upCTKGX2Lgk'
+    template_id = 'nJd-Uw5WlA9DBDqYPTSmpwhysbE269qMP5Adx-dbyyU'
     
-    data = {'first': {'value': '您的宽带即将到期', 'color': '#173177'}, 'keyword1': {'value': '账号'}, 'keyword2': {'value': '2020-01-01 08:00'}, 'remark': {'value': '如需继续使用, 请点击充值'}}
+    data = {'first': {'value': '您的宽带即将到期', 'color': '#173177'}, 'keyword1': {'value': '账号'}, 'keyword2': {'value': '到期时间 2020-01-01 08:00'}, 'remark': {'value': '如需继续使用, 请点击充值'}}
     a.test_send_template(openid, template_id, data, url=WeClient.recharge_uri)
     """)
 
