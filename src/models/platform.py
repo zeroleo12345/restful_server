39e73c7b3a022568fc37153670922a401fa85208
zeroleo12345 <zeroleo12345@163.com>
2021-01-12 20:58:24 +0800

    add owner_profit_percent

--- src/models/platform.py
+++ src/models/platform.py
@@ -15,6 +15,7 @@ class Platform(models.Model, BaseModel):
     owner_user_id = models.IntegerField()
     ssid = models.CharField(max_length=255, null=True)
     qrcode_content = models.URLField(max_length=512, null=True)     # 二维码内容, 例如: http://weixin.qq.com/q/02SE2_xxx
+    owner_profit_percent = models.DecimalField(null=True, max_digits=9, decimal_places=6)   # 利润比例.(单位%, 例如 30%)
     #
     created_at = models.DateTimeField(auto_now_add=True)    # auto_now_add only generated on 新创建
     updated_at = models.DateTimeField(auto_now=True)        # auto_now is generated on 每次修改
