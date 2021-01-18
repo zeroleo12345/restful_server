3aa0d8a7bb3d515fc784b6875b1d5858686e490e
zeroleo12345 <zeroleo12345@163.com>
2021-01-18 22:51:00 +0800

    m

--- src/models/resource_change.py
+++ src/models/resource_change.py
@@ -11,7 +11,7 @@ class ResourceChange(models.Model, BaseModel):
 
     id = models.AutoField(primary_key=True)
     user_id = models.BigIntegerField()
-    order_id = models.IntegerField()        # TODO 删除该字段
+    order_id = models.IntegerField(null=True)        # TODO 删除该字段
     out_trade_no = models.CharField(max_length=255, null=True)        # 商家订单号 out_trade_no. TODO 删除null=True
     before = models.DateTimeField()
     after = models.DateTimeField()
