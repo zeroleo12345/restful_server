6d68f41d93706ddbd6db8ec2349a0bb1074a16bd
zeroleo12345 <zeroleo12345@163.com>
2021-01-18 22:46:28 +0800

    m

--- src/views/resource/views.py
+++ src/views/resource/views.py
@@ -4,6 +4,7 @@ from framework.authorization import JWTAuthentication
 from controls.auth import Authentication
 from framework.restful import BihuResponse
 from models import Account, User
+from trade.settings import log
 
 
 class UserResourceView(APIView):
@@ -14,6 +15,7 @@ class UserResourceView(APIView):
     def get(self, request):
         auth = Authentication(request)
         #
+        log.i(f'user_id: {auth.user_id}')
         user = User.get(user_id=auth.user_id)
         account = Account.get(user_id=user.user_id, platform_id=user.bind_platform_id)
         data = account.to_dict()
