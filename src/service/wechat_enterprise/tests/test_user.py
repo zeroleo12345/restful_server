8b0047b653ff87a67f1707e700fe91c1b5472b20
zeroleo12345 <zeroleo12345@163.com>
2021-02-06 23:15:07 +0800

    fix

--- src/service/wechat_enterprise/tests/test_user.py
+++ src/service/wechat_enterprise/tests/test_user.py
@@ -18,4 +18,5 @@ class Test(object):
 
     @pytest.mark.skip(reason='')
     def test_send_template(self, user_id: str):
+        # {'errcode': 0, 'errmsg': 'ok', 'openid': 'oV4Up1vv8-PqS4uR6SsyAFJ-N9ds'}
         return WeUser.convert_to_openid(user_id=user_id)
