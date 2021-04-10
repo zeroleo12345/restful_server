d72b0a0a42077b26a5ef0599ed225eb8376a0372
zeroleo12345 <zeroleo12345@163.com>
2021-04-10 16:44:44 +0800

    fix

--- src/trade/models/factories/user.py
+++ src/trade/models/factories/user.py
@@ -1,18 +1,40 @@
+import pytest
+from unittest.mock import patch
 from rest_framework import status
+from trade.models import Platform
 #
-from trade.models import Account
+from trade.models import Account, User
 
 
 class UserFactory(object):
     @classmethod
     def new_user_and_authorization(cls, client):
-        data = {
-            'code': '001yROix1KtF1c0waVgx1k6Bix1yROiR'
-        }
-        # TODO 注册流程 改为 扫描带参数二维码!
-        response = client.get('/user', data=data)
-        assert response.status_code == status.HTTP_200_OK
+        platform = Platform.get(owner_user_id=1)
+        if not platform:
+            platform = Platform.create(owner_user_id=1)
+            platform.update(platform_id=platform.id, ssid=f'WIFI-{platform.platform_id}')
+        from wechatpy.events import SubscribeScanEvent
+        from collections import OrderedDict
         #
+        from_user_openid = 'o0FSR0RqA2oXc6x1uW0MGVKayp'
+        with patch('service.wechat.we_crypto.WeCrypto.decrypt_and_parse_message') as _mock:
+            _mock.return_value = SubscribeScanEvent(OrderedDict([
+                ('ToUserName', 'gh_d71de39dd1'),
+                ('FromUserName', from_user_openid),
+                ('CreateTime', '1617715451'),
+                ('MsgType', 'event'),
+                ('Event', 'subscribe_scan'),
+                ('EventKey', f'{platform.platform_id}'),
+                ('Ticket', 'nFxLmNvbS9xLzAydXI0bHQ3Z2tlaGgxMDAwME0wN2cAAgTl_-pfAw')
+            ]))
+            data = {
+                'body': 'xxx'
+            }
+            response = client.post('/mp/echostr', data=data)
+            assert response.status_code == status.HTTP_200_OK
+        #
+        # TODO 注册流程 改为 扫描带参数二维码!
+        user = User.get(openid=from_user_openid)
         res_dict = response.json()
         assert 'ok' == res_dict['code']
         assert 'user' in res_dict['data']
